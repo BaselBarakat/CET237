b55dad8fec737780ac11f919efc90f130285a754
Basel Barakat <eng.basel_barakat@live.com>
2022-03-04 17:07:20 +0000

    Update time_print.py

--- time_print.py
+++ time_print.py
@@ -1,6 +1,11 @@
 import datetime
-now = datetime.datetime.now()
 
-print("Current date and time: ")
-print(now.strftime('%Y-%m-%d %H:%M:%S'))
-print(now.strftime('%H:%M:%S on %A, %B the %dth, %Y'))
+def main()
+  now = datetime.datetime.now()
+
+  print("Current date and time: ")
+  print(now.strftime('%Y-%m-%d %H:%M:%S'))
+  print(now.strftime('%H:%M:%S on %A, %B the %dth, %Y'))
+  
+if __name__ == '__main__':
+  main()
